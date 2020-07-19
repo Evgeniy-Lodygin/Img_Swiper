dd918c06efad27c1765f9a5ae3421e94d380aa77
Evgeniy Lodygin <evgeniy.lodygin@gmail.com>
2020-07-19 19:43:34 +0300

    Project is done
    I've already finished it.
    You can click it as you wish and turn slide show on/off at any moment

--- scripts/ImgSwiper2.js
+++ scripts/ImgSwiper2.js
@@ -5,9 +5,13 @@ class ImgSwiper{
 		this.swiperBtn = this.swiper.find(".swiper-btn-link");
 		this.firstPic = this.swiper.find(".swiper-img:first-child");
 		this.prevHighlightBtn = this.swiper.find(".swiper-btn-link:first-child");
+		this.slideshowBtn = this.swiper.find(".slideshowBtn");
 		this.currentPictureLink = null;
-		this.previousImgNum = 0;
 		this.currentImgNum = 0;
+		this.currentImgNumSlide = 0; //temp
+		this.SetIntervalStart 		= 0;
+		this.slideshowPeriod 		= 1;
+		this.isSlideshowStart 		= false;
 		this.showInit();
 		this.highlightInit();
 		this.createEvents();
@@ -22,16 +26,47 @@ imgChange(event){
 	event.preventDefault();
 	this.currentPictureLink = $(event.currentTarget);
 	this.currentImgNum = this.swiperBtn.index(this.currentPictureLink);
-	this.firstPic.removeClass("b-show");
+	this.swiperImg.filter(".b-show").removeClass("b-show");
 	this.swiperImg.eq(this.currentImgNum).addClass("b-show");
 	this.firstPic = this.swiperImg.eq(this.currentImgNum)
-	this.prevHighlightBtn.removeClass("highlight");
+	this.swiperBtn.filter(".highlight").removeClass("highlight");
 	this.currentPictureLink.addClass("highlight");
-	this.prevHighlightBtn = this.currentPictureLink;
-	this.previousImgNum = this.currentImgNum;
 }
+slideImg(event, shift){
+	this.currentImgNumSlide = this.swiperImg.index(this.swiperImg.filter(".b-show"));
+
+	if(this.currentImgNumSlide == this.swiperImg.length -1){ //last one element
+		this.currentImgNumSlide = 0;
+		this.swiperImg.eq(this.swiperImg.length -1).removeClass("b-show");
+		this.swiperImg.eq(this.currentImgNumSlide).addClass("b-show");
+		this.swiperBtn.eq(this.swiperImg.length -1).removeClass("highlight");
+		this.swiperBtn.eq(this.currentImgNumSlide).addClass("highlight");
+	}
+	else{
+		this.swiperImg.eq(this.currentImgNumSlide).removeClass("b-show");
+		this.swiperImg.eq(this.currentImgNumSlide + shift).addClass("b-show");
+		this.swiperBtn.eq(this.currentImgNumSlide).removeClass("highlight");
+		this.swiperBtn.eq(this.currentImgNumSlide + shift).addClass("highlight");
+	}
+}
+	slideshow(){
+		if(this.isSlideshowStart){
+			this.stopSlideshow();
+		}
+		else{
+			this.slideshowBtn.text("stop");
+			this.isSlideshowStart = true;
+			this.SetIntervalStart = setInterval(() => {this.slideImg(null, 1)}, this.slideshowPeriod * 1000);//slideshow starts
+		}
+	}
+	stopSlideshow(){
+			this.slideshowBtn.text("start");
+			this.isSlideshowStart = false;
+			clearInterval(this.SetIntervalStart);
+	}
 	createEvents(){
 		this.swiperBtn.click(this.imgChange.bind(this));
+		this.slideshowBtn.click(this.slideshow.bind(this));
 	}
 }
 let swipe = new ImgSwiper("#container-imgswiper");
